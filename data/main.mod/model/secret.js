23b1edf132a79dba498e4f6c91ab3bbad381fdef
Benoit Marchant <marchant@mac.com>
2024-05-01 23:19:37 -0700

    -> mod

--- data/main.mod/model/secret.js
+++ data/main.mod/model/secret.js
@@ -2,7 +2,7 @@
     @module app-infrastructure-data-mod/data/main.mod/model/aws/secret
 */
 
-var DataObject = require("montage/data/model/data-object").DataObject;
+var DataObject = require("mod/data/model/data-object").DataObject;
 
 /**
  * @class Secret
