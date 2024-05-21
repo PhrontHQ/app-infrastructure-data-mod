d447837e5517340659732634701f3b28bc9b2202
marchant <marchant@mac.com>
2024-05-20 23:39:09 -0700

    Fix object descriptors and class

--- data/main.mod/model/user-session.js
+++ data/main.mod/model/user-session.js
@@ -6,62 +6,16 @@ var DataObject = require("mod/data/model/data-object").DataObject;
  */
 exports.UserSession = DataObject.specialize(/** @lends UserSession.prototype */ {
 
-    username: {
+    identity: {
         value: undefined
     },
-    password: {
+    environment: {
         value: undefined
     },
-    accountConfirmationCode: {
+    connectionId: {
         value: undefined
     },
-    isAccountConfirmed: {
+    connectionTimeRange: {
         value: false
-    },
-    isAuthenticated: {
-        get: function () {
-            return !!this.session;
-        },
-        set: function (value) {
-            if (this.session && !value) {
-                this.session = null;
-            }
-        }
-    },
-    isMfaEnabled: {
-        value: false
-    },
-    firstName: {
-        value: undefined
-    },
-    lastName: {
-        value: undefined
-    },
-    email: {
-        value: undefined
-    },
-    phone: {
-        value: undefined
-    },
-    image: {
-        value: undefined
-    },
-    tags: {
-        value: undefined
-    },
-    idToken: {
-        value: undefined
-    },
-    accessToken: {
-        value: undefined
-    },
-    mfaCode: {
-        value: undefined
-    },
-    locale: {
-        value: undefined
-    },
-    timeZone: {
-        value: undefined
     }
 });
